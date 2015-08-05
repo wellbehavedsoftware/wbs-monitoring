65aa88e5e242a61e589993be2a524e3a443901c9
James Pharaoh <james@pharaoh.uk>
2015-08-05 17:16:30 +0200

    make check_site handle headers, remove html checks

--- src/check_site.rs
+++ src/check_site.rs
@@ -24,6 +24,7 @@ struct Opts {
 	uri: String,
 	text: String,
 	secure: bool,
+	headers: Vec<String>,
 }
 
 fn parse_options () -> Option<Opts> {
@@ -32,7 +33,7 @@ fn parse_options () -> Option<Opts> {
 
 	let mut opts = Options::new();
 
-	opts.optflag (	
+	opts.optflag (
 			"h",
 			"help",
 			"print this help menu");
@@ -40,13 +41,13 @@ fn parse_options () -> Option<Opts> {
 	opts.reqopt (
 			"H",
 			"hostname",
-			"Hostname of the site in which te check is performed",
+			"Hostname of the site in which the check is performed",
 			"<hostname>");
 
 	opts.reqopt (
 			"u",
 			"uri",
-			"URI of the site in which te check is performed",
+			"URI of the site in which the check is performed",
 			"<url>");
 
 	opts.reqopt (
@@ -61,6 +62,11 @@ fn parse_options () -> Option<Opts> {
 			"use https instead of http",
 			"<http-enabled>");
 
+	opts.optmulti (
+			"",
+			"header",
+			"Request header to send, such as 'Name: value'",
+			"HEADER");
 
 	let matches = match opts.parse (args) {
 		Ok (m) => { m }
@@ -79,6 +85,7 @@ fn parse_options () -> Option<Opts> {
 	let uri = matches.opt_str ("uri").unwrap ();
 	let text = matches.opt_str ("text").unwrap ();
 	let secure_str = matches.opt_str ("ssl").unwrap ();
+	let headers = matches.opt_strs ("header");
 
 	let mut secure: bool = false;
 	if &secure_str == "true" {
@@ -90,47 +97,43 @@ fn parse_options () -> Option<Opts> {
 		uri: uri,
 		text: text,
 		secure: secure,
+		headers: headers,
 	});
 
 }
 
-fn get_response (host: &str, uri: &str, secure: bool) -> String {
+fn get_response (host: &str, uri: &str, secure: bool, headers: &Vec<String>) -> String {
 
-	let mut response: String;
+	let mut list_command =
+		process::Command::new ("/usr/lib/nagios/plugins/check_http");
+
+	list_command
+		.arg ("-H".to_string ())
+		.arg (host.to_string ())
+		.arg ("-u".to_string ())
+		.arg (uri.to_string ());
 
 	if secure {
 
-		let list_output =
-			match process::Command::new ("/usr/lib/nagios/plugins/check_http")
-				.arg ("-H".to_string ())
-				.arg (host.to_string ())
-				.arg ("-u".to_string ())
-				.arg (uri.to_string ())
-				.arg ("--ssl".to_string())
-				.output () {
-			Ok (output) => { output }
-			Err (err) => { return format!("SITE-UNKNOWN: Error while checking the site: {}", err); }
-		};
-
-		response = String::from_utf8_lossy(&list_output.stdout).to_string()
-	}
-
-	else {
-	
-		let list_output =
-			match process::Command::new ("/usr/lib/nagios/plugins/check_http")
-				.arg ("-H".to_string ())
-				.arg (host.to_string ())
-				.arg ("-u".to_string ())
-				.arg (uri.to_string ())
-				.output () {
-			Ok (output) => { output }
-			Err (err) => {return format!("SITE-UNKNOWN: Error while checking the site: {}", err); }
-		};
-
-		response = String::from_utf8_lossy(&list_output.stdout).to_string()
+		list_command
+			.arg ("--ssl".to_string ());
 
 	}
+
+	for header in headers {
+
+		list_command
+			.arg ("--header")
+			.arg (header);
+
+	}
+
+	let list_output = match list_command.output () {
+		Ok (output) => { output }
+		Err (err) => { return format!("SITE-UNKNOWN: Error while checking the site: {}", err); }
+	};
+
+	let response = String::from_utf8_lossy(&list_output.stdout).to_string();
 
 	let response_vector: Vec<&str> = response.split(' ').collect();
 
@@ -185,7 +188,7 @@ fn in_array (element: isize, haystack: Vec<isize>) -> bool {
 	return found;
 }
 
-fn check_text (host: &str, uri: &str, text: &str, secure: bool) -> String {
+fn check_text (host: &str, uri: &str, text: &str, secure: bool, headers: &Vec<String>) -> String {
     
 	let mut prefix: String;
 
@@ -194,25 +197,34 @@ fn check_text (host: &str, uri: &str, text: &str, secure: bool) -> String {
 
 	let url = prefix + host + uri;
 	
-   	let resp = http::handle()
-			.get(url)
-			.header("Accept-Language", "en")
-			.exec().unwrap();
+   	let mut http_handle = http::handle();
+
+	let mut http_request = http_handle.get (url);
+
+	http_request = http_request.header("Accept-Language", "en");
+
+	for header in headers {
+		let header_parts: Vec<&str> = header.splitn (2, ": ").collect ();
+		http_request = http_request.header (header_parts [0], header_parts [1]);
+	}
+
+	let resp = http_request.exec().unwrap();
 
 	let url_code = String::from_utf8_lossy(resp.get_body());
 
- 	if url_code.contains(text) && url_code.contains("<body") && url_code.contains("</body>") 
+	if url_code.contains(text)
+	/* && url_code.contains("<body") && url_code.contains("</body>") 
 		&& url_code.contains("<head") && url_code.contains("</head>") && url_code.contains("<html") 
-		&& url_code.contains("</html>") { 
+		&& url_code.contains("</html>")*/ { 
 	
-		return "SITE-OK: Correct HTML structure and text found.".to_string();
+		return "SITE-OK: Correct text found.".to_string();
 
-	 } 
-	else { 
+	} else {
 
-		return "SITE-WARNING: Incorrect HTML structure or text not found.".to_string(); 
+		return "SITE-WARNING: Text not found.".to_string(); 
 
-	} 	
+	}
+
 }
 
 fn main () {
@@ -230,9 +242,10 @@ fn main () {
 	let uri = &opts.uri;
 	let text = &opts.text;
 	let secure = opts.secure;
+	let headers = opts.headers;
 
-	let response_res = get_response(hostname, uri, secure);
-	let text_res = check_text(hostname, uri, text, secure);
+	let response_res = get_response(hostname, uri, secure, & headers);
+	let text_res = check_text(hostname, uri, text, secure, & headers);
 
 
 	if response_res.contains("UNKNOWN") || text_res.contains("UNKNOWN") {
